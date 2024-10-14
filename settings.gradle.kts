b77656357c0b3cd98954ae03b1d36061ce466e6f
Sorrow Blue <sorrowblue.dev@gmail.com>
2024-10-14 18:13:11 +0900

    :construction: Work in progress

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("com.android.settings") version "8.7.0"
 }
 
