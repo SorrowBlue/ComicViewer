a42d59fa45aa5e01ee0f85536c600e3bd287175a
Sorrow Blue <sorrowblue.dev@gmail.com>
2024-11-17 13:43:09 +0900

    :fire: Delete old Fetcher

--- data/coil/src/main/java/com/sorrowblue/comicviewer/data/coil/di/SingletonBindsModule.kt
+++ data/coil/src/main/java/com/sorrowblue/comicviewer/data/coil/di/SingletonBindsModule.kt
@@ -2,17 +2,13 @@ package com.sorrowblue.comicviewer.data.coil.di
 
 import coil3.fetch.Fetcher
 import com.sorrowblue.comicviewer.data.coil.book.BookThumbnailFetcher
-import com.sorrowblue.comicviewer.data.coil.book.OldBookThumbnailFetcher
 import com.sorrowblue.comicviewer.data.coil.favorite.FavoriteThumbnailFetcher
 import com.sorrowblue.comicviewer.data.coil.folder.FolderThumbnailFetcher
-import com.sorrowblue.comicviewer.data.coil.folder.OldFolderThumbnailFetcher
 import com.sorrowblue.comicviewer.data.coil.impl.ImageCacheDataSourceImpl
 import com.sorrowblue.comicviewer.data.coil.page.BookPageFetcher
 import com.sorrowblue.comicviewer.domain.model.BookPageRequest
 import com.sorrowblue.comicviewer.domain.model.favorite.Favorite
-import com.sorrowblue.comicviewer.domain.model.file.Book
 import com.sorrowblue.comicviewer.domain.model.file.BookThumbnail
-import com.sorrowblue.comicviewer.domain.model.file.Folder
 import com.sorrowblue.comicviewer.domain.model.file.FolderThumbnail
 import com.sorrowblue.comicviewer.domain.service.datasource.ImageCacheDataSource
 import dagger.Binds
@@ -31,15 +27,7 @@ internal interface SingletonBindsModule {
 
     @Singleton
     @Binds
-    fun bindOldBookThumbnailFetcher(factory: OldBookThumbnailFetcher.Factory): Fetcher.Factory<Book>
-
-    @Singleton
-    @Binds
     fun bindBookThumbnailFetcher(factory: BookThumbnailFetcher.Factory): Fetcher.Factory<BookThumbnail>
-
-    @Singleton
-    @Binds
-    fun bindOldFolderThumbnailFetcher(factory: OldFolderThumbnailFetcher.Factory): Fetcher.Factory<Folder>
 
     @Singleton
     @Binds
