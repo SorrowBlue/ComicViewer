323bb52a7e473bddd9343a46502e7f724b09281f
Sorrow Blue <sorrowblue.dev@gmail.com>
2024-12-10 14:25:27 +0900

    :construction: Work in progress (#626)

--- build-logic/src/main/kotlin/AndroidLintConventionPlugin.kt
+++ build-logic/src/main/kotlin/AndroidLintConventionPlugin.kt
@@ -5,7 +5,6 @@ import com.android.build.api.dsl.Lint
 import com.sorrowblue.comicviewer.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.logging.Logging
 import org.gradle.api.plugins.PluginManager
 import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.configure
