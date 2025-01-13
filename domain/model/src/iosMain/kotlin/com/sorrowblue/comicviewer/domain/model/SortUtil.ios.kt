abb6156f55dee6cd790177052a75836dc3cbcdec
Sorrow Blue <sorrowblue.dev@gmail.com>
2025-01-13 22:50:03 +0900

    :construction: Fix common source

---
+++ domain/model/src/iosMain/kotlin/com/sorrowblue/comicviewer/domain/model/SortUtil.ios.kt
@@ -0,0 +1,21 @@
+package com.sorrowblue.comicviewer.domain.model
+
+import com.sorrowblue.comicviewer.domain.model.file.File
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual object SortUtil {
+    actual val compareFile: Comparator<File>
+        get() = Comparator<File> { a, b -> TODO("Not yet implemented") }
+
+    actual fun filter(
+        file: File,
+        supportExtensions: List<String>,
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    actual fun sortedIndex(list: List<File>): List<File> {
+        TODO("Not yet implemented")
+    }
+
+}
