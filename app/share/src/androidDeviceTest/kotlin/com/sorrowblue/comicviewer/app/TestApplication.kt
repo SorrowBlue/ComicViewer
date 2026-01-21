5901640c461c565bbc8a1fe7736abb3232eb147d
SorrowBlue <sorrowblue.dev@gmail.com>
2026-01-21 13:14:44 +0900

    :art: Introduce custom InstrumentationTestRunner and TestApplication for improved testing setup

---
+++ app/share/src/androidDeviceTest/kotlin/com/sorrowblue/comicviewer/app/TestApplication.kt
@@ -0,0 +1,30 @@
+package com.sorrowblue.comicviewer.app
+
+import android.app.Application
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.work.Configuration
+import com.sorrowblue.comicviewer.feature.settings.info.license.LicenseeHelper
+import com.sorrowblue.comicviewer.framework.common.getPlatformGraph
+import dev.zacsweers.metro.createGraphFactory
+
+class TestApplication : Application(),
+    Configuration.Provider  {
+
+    private val appGraph by lazy {
+        createGraphFactory<AppGraph.Factory>().createAppGraph(
+            InstrumentationRegistry.getInstrumentation().context,
+            object : LicenseeHelper {
+                override suspend fun loadLibraries(): ByteArray {
+                    TODO("Not yet implemented")
+                }
+            })
+    }
+
+    init {
+        getPlatformGraph = { appGraph }
+    }
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder().setWorkerFactory(appGraph.workerFactory).build()
+
+}
