d65455702792881999df6391c369749dc5090103
SorrowBlue <sorrowblue.dev@gmail.com>
2026-04-09 11:15:47 +0900

    :refactor: Simplify database builder by removing context dependency in TestDatabaseHelper

--- data/database/src/androidHostTest/kotlin/com/sorrowblue/comicviewer/data/database/TestDatabaseHelper.android.kt
+++ data/database/src/androidHostTest/kotlin/com/sorrowblue/comicviewer/data/database/TestDatabaseHelper.android.kt
@@ -2,13 +2,11 @@ package com.sorrowblue.comicviewer.data.database
 
 import androidx.room3.Room
 import androidx.room3.RoomDatabase
-import androidx.test.platform.app.InstrumentationRegistry
 import dev.zacsweers.metro.Inject
 
 @Inject
 internal actual class TestDatabaseHelper {
     actual fun getDatabaseBuilder(): RoomDatabase.Builder<ComicViewerDatabase> {
-        val context = InstrumentationRegistry.getInstrumentation().context
-        return Room.inMemoryDatabaseBuilder<ComicViewerDatabase>(context)
+        return Room.inMemoryDatabaseBuilder<ComicViewerDatabase>()
     }
 }
