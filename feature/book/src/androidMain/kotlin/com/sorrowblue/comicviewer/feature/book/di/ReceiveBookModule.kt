eee1916d87aace4078e65613acce5da74fbddc47
SorrowBlue <sorrowblue.dev@gmail.com>
2026-07-16 14:41:41 +0900

    :refactor: update module contributions to use AppScope for improved dependency management in ReceiveBook and Book modules

--- feature/book/src/androidMain/kotlin/com/sorrowblue/comicviewer/feature/book/di/ReceiveBookModule.kt
+++ feature/book/src/androidMain/kotlin/com/sorrowblue/comicviewer/feature/book/di/ReceiveBookModule.kt
@@ -7,16 +7,16 @@ package com.sorrowblue.comicviewer.feature.book.di
 import com.sorrowblue.comicviewer.feature.book.navigation.ReceiveBookNavKey
 import com.sorrowblue.comicviewer.feature.book.navigation.receiveBookNavEntry
 import com.sorrowblue.comicviewer.feature.book.receive.ReceiveBookScreenContext
-import com.sorrowblue.comicviewer.framework.ui.navigation.NavScope
 import com.sorrowblue.comicviewer.framework.ui.navigation.asEntry
 import com.sorrowblue.comicviewer.framework.ui.navigation3.NavKeyEntry
 import com.sorrowblue.comicviewer.framework.ui.navigation3.ScreenEntryProvider
+import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.ElementsIntoSet
 import dev.zacsweers.metro.IntoSet
 import dev.zacsweers.metro.Provides
 
-@ContributesTo(NavScope::class)
+@ContributesTo(AppScope::class)
 interface ReceiveBookModule {
     @Provides
     @ElementsIntoSet
--- feature/book/src/commonMain/kotlin/com/sorrowblue/comicviewer/feature/book/di/BookModule.kt
+++ feature/book/src/commonMain/kotlin/com/sorrowblue/comicviewer/feature/book/di/BookModule.kt
@@ -9,16 +9,16 @@ import com.sorrowblue.comicviewer.feature.book.menu.BookMenuScreenContext
 import com.sorrowblue.comicviewer.feature.book.navigation.BookMenuNavKey
 import com.sorrowblue.comicviewer.feature.book.navigation.bookMenuNavEntry
 import com.sorrowblue.comicviewer.feature.book.navigation.bookNavEntry
-import com.sorrowblue.comicviewer.framework.ui.navigation.NavScope
 import com.sorrowblue.comicviewer.framework.ui.navigation.asEntry
 import com.sorrowblue.comicviewer.framework.ui.navigation3.NavKeyEntry
 import com.sorrowblue.comicviewer.framework.ui.navigation3.ScreenEntryProvider
+import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.ElementsIntoSet
 import dev.zacsweers.metro.IntoSet
 import dev.zacsweers.metro.Provides
 
-@ContributesTo(NavScope::class)
+@ContributesTo(AppScope::class)
 interface BookModule {
     @Provides
     @ElementsIntoSet
