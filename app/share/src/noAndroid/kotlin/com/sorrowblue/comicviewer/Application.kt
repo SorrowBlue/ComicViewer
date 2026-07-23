6c685a5ef921742aea48facf047fdf642bb23065
SorrowBlue <sorrowblue.dev@gmail.com>
2026-07-24 03:49:40 +0900

    :refactor: enhance Application composable by integrating LocalMetroViewModelFactory for improved ViewModel management

--- app/share/src/noAndroid/kotlin/com/sorrowblue/comicviewer/Application.kt
+++ app/share/src/noAndroid/kotlin/com/sorrowblue/comicviewer/Application.kt
@@ -17,21 +17,26 @@ import com.sorrowblue.comicviewer.framework.common.Initializer
 import com.sorrowblue.comicviewer.framework.common.PlatformContext
 import com.sorrowblue.comicviewer.framework.common.require
 import com.sorrowblue.comicviewer.framework.designsystem.theme.LocalDarkMode
+import dev.zacsweers.metrox.viewmodel.LocalMetroViewModelFactory
 import dev.zacsweers.metrox.viewmodel.metroViewModel
+import logcat.LogPriority
+import logcat.logcat
 
 @Composable
 context(context: PlatformContext, appGraph: AppGraph)
 fun Application(finishApp: () -> Unit) {
-    val viewModel = metroViewModel<ApplicationViewModel>()
-    val state = rememberComicViewerUIState()
-    val darkMode by viewModel.displaySettings.collectAsStateWithLifecycle(DarkMode.DEVICE)
-    CompositionLocalProvider(LocalDarkMode provides darkMode) {
-        ComicViewerUI(finishApp = finishApp, state = state)
-    }
-    LaunchedEffect(Unit) {
-        Initializer.initialize(
-            context.require<InitializerContext.Factory>()
-                .createInitializerContext().initializer.toList(),
-        )
+    CompositionLocalProvider(LocalMetroViewModelFactory provides appGraph.metroVmf) {
+        val viewModel = metroViewModel<ApplicationViewModel>()
+        val state = rememberComicViewerUIState()
+        val darkMode by viewModel.displaySettings.collectAsStateWithLifecycle(DarkMode.DEVICE)
+        CompositionLocalProvider(LocalDarkMode provides darkMode) {
+            ComicViewerUI(finishApp = finishApp, state = state)
+        }
+        LaunchedEffect(Unit) {
+            Initializer.initialize(
+                context.require<InitializerContext.Factory>()
+                    .createInitializerContext().initializer.toList(),
+            )
+        }
     }
 }
